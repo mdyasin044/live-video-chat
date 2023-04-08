3731f3404b442a330705d2cfc40a3db476dba446
yasin islam <yasinbuetcse14@gmail.com>
2023-04-08 14:25:08 +0600

    changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "https://live-video-chat.onrender.com",
+		origin: "http://localhost:3000",
 		methods: [ "GET", "POST" ]
 	}
 })
