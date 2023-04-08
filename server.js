0313af15f8b88503a0d28fd23b3f2ccdc0df3e9a
yasin islam <yasinbuetcse14@gmail.com>
2023-04-08 18:44:45 +0600

    change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "https://live-video-chat.onrender.com",
+		origin: "https://rainbow-semifreddo-353473.netlify.app/",
 		methods: [ "GET", "POST" ]
 	}
 })
